777583ae391bb0ebf81ea34f120c2a7eacecb326
arthur <arthur.cott@outlook.fr>
2023-11-23 14:45:46 +0100

    carte comparaison

--- classes/carte.py
+++ classes/carte.py
@@ -1,7 +1,7 @@
 SYMBOLES = ["trèfle", "pique", "carreau", "coeur"]
 
 class Carte():
-    def __init__(self, valeur, symbole) -> None:
+    def __init__(self, valeur, symbole: SYMBOLES) -> None:
         if symbole not in SYMBOLES:
             raise RuntimeError("Symbole invalide")
         self.valeur = valeur
@@ -11,5 +11,23 @@ class Carte():
         else:
             self.couleur = "rouge"
 
-    def __lt__(self):
-        pass
+    def __lt__(self, carte):
+        if self.valeur == carte.valeur:
+            return None
+        elif self.valeur == 1:
+            return False
+        elif carte.valeur == 1:
+            return True
+        elif self.valeur < carte.valeur:
+            return True
+        else: 
+            return False
+        
+    def __str__(self) -> str:
+        return f"| {self.valeur} {self.symbole} |"
+        
+if __name__ == "__main__":
+    liste = [Carte(4, "trèfle"), Carte(1, "pique"), Carte(10, "coeur"), Carte(1, "carreau")]
+    print(liste[1]<liste[2])
+    for carte in liste:
+        print(carte)
