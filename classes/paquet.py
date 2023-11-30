a0974c1ab96f1b0d864c73a541b013cf350ab1b2
Pierre <pierre.lopes888@gmail.com>
2023-11-30 14:28:26 +0100

    Documentation de paquet

--- classes/paquet.py
+++ classes/paquet.py
@@ -2,6 +2,9 @@ from carte import Carte
 from random import randint
 
 class Paquet():
+   '''Classe de paquet
+   Attributs :
+        cartes(list): Liste de cartes crées dans la méthode Carte''' 
     def __init__(self) -> None:
         self.cartes = []
 
@@ -9,6 +12,8 @@ class Paquet():
         self.cartes.append(carte)
 
     def melanger(self):
+        """Mélange le paquet initial en mettant le résultat dans une boucle
+        """
         paquet_melange = []
         for i in range (0,len(self.cartes)):
             k=randint(0,len(self.cartes)-1)
@@ -16,6 +21,12 @@ class Paquet():
         self.cartes = paquet_melange
 
     def split(self):
+        """Divise le paquet de 52 cartes mélangé en 2 paquets de 26 cartes chacuns
+
+
+        Returns:
+            Lists: Renvoie les listes des 2 nouveaux paquets créés.
+        """
         paquet_1 = Paquet()
         paquet_2 = Paquet()     
         for i in range(0,len(self.cartes)):
@@ -24,13 +35,18 @@ class Paquet():
             else:
                 paquet_2.ajouter(self.cartes[i])
         return paquet_1, paquet_2
-    
+
     def __str__(self) -> str:
+        """Affichage des cartes dans le paquet
+
+        Returns:
+            str: Affiche les cartes
+        """
         return_str = "(\n"
         for carte in self.cartes:
             return_str += f"{str(carte)}\n"
         return return_str + ")"
-        
+
 if __name__ == "__main__":
     liste = []
     for i in range(1, 53):
