adba0c487ab6dbde3068808423631228e990d2df
arthur <arthur.cott@outlook.fr>
2023-12-07 13:36:15 +0100

    documentation jeu

--- fonctions/jeu.py
+++ fonctions/jeu.py
@@ -1,10 +1,16 @@
 from classes.carte import Carte, SYMBOLES
 from classes.paquet import Paquet
 
-def creation_paquet_0():
+def creation_paquet_0()->Paquet:
+    """Créer et retourne le paquet de départ (0)
+
+    Returns:
+        Paquet: Paquet de départ
+    """
     paquet_0 = Paquet()
     for symbole in SYMBOLES:
         for valeur in range(1,14):
             paquet_0.ajouter(Carte(symbole, valeur))
     return paquet_0
+
 
