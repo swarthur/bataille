44ef80e474ccbc3b153e15cb93a23ead5632a698
arthur <arthur.cott@free.fr>
2023-12-10 18:23:11 +0100

    Docstrigs terminées + suppr. print supperflu dans les tests

--- bataille/classes/paquet.py
+++ bataille/classes/paquet.py
@@ -1,5 +1,7 @@
 from bataille.classes.carte import Carte
 from random import randint
+from typing_extensions import Self
+
 
 class Paquet():
     """Paquet de cartes
@@ -7,40 +9,64 @@ class Paquet():
         cartes(list): liste de cartes
     """
     def __init__(self, cartes:list[Carte]) -> None:
+        """Constructeur de Paquet
+
+        Args:
+            cartes (list[Carte]): Liste de cartes à ajouter
+        """
         self.cartes = cartes
 
     def ajouter(self, carte: Carte):
+        """Ajoute une carte au paquet
+
+        Args:
+            carte (Carte): Carte à ajouter au paquet
+        """
         self.cartes.append(carte)
 
-    def melanger(self):
+    def melanger(self)-> None:
         """
         Mélange le paquet initial en mettant le résultat dans une boucle
         """
         paquet_melange = []
-        for i in range (0,len(self.cartes)):
-            k=randint(0,len(self.cartes)-1)
-            paquet_melange.append(self.cartes.pop(k))
+        for carte in range (0,len(self.cartes)):
+            carte_rand=randint(0,len(self.cartes)-1)
+            paquet_melange.append(self.cartes.pop(carte_rand))
         self.cartes = paquet_melange
 
-    def tete(self):
+    def tete(self)-> Carte:
+        """Retourne la carte en haut du paquet
+
+        Returns:
+            Carte: Carte en haut du paquet
+        """
         if not self.est_vide():
             return self.cartes[-1]
 
-    def recup(self, paquet_adv):
+    def recup(self, paquet_adv: Self)-> None:
         carte_recup = paquet_adv.cartes.pop(-1)
         self.cartes.insert(0, carte_recup)
 
-    def est_vide(self):
+    def est_vide(self)-> bool:
+        """Teste si le paquet est vide
+
+        Returns:
+            bool: Paquet vide ou non
+        """
         if len(self) == 0:
             return True
         else:
             return False
 
-    def split(self):
+    def split(self)-> tuple[Self, Self]:
         """Divise le paquet de X cartes en 2 paquets de X/2 cartes chacuns
 
+        Raises:
+            RuntimeError: Erreur si le nombre de cartes est impair
+            RuntimeError: Erreur si le paquet est vide
+
         Returns:
-            tuple: Renvoie un tuple contenant les 2 nouveaux paquets créés.
+            tuple[Self, Self]: Renvoie un tuple contenant les 2 nouveaux paquets créés.
         """
         if len(self.cartes)%2 != 0:
             raise RuntimeError("Nombre de cartes impair")
@@ -59,12 +85,17 @@ class Paquet():
         """Affichage des cartes dans le paquet
 
         Returns:
-            str: Affiche les cartes
+            str: Chaine de caractère des cartes du paquet
         """
         return_str = "(\n"
         for carte in self.cartes:
             return_str += f"{str(carte)}\n"
         return return_str + ")"
     
-    def __len__(self):
+    def __len__(self)-> int:
+        """Retourne le nombre de cartes du paquet
+
+        Returns:
+            int: Nombre de cartes du paquet
+        """
         return len(self.cartes)
