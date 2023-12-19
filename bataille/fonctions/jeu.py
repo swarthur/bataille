6f9011458b9d11f998dc82fd86248ba1c23d9120
arthur <arthur.cott@free.fr>
2023-12-19 13:54:33 +0100

    Fix bataille et ajout nb_cartes

--- bataille/fonctions/jeu.py
+++ bataille/fonctions/jeu.py
@@ -1,28 +1,32 @@
 from bataille.classes.carte import Carte, SYMBOLES
 from bataille.classes.paquet import Paquet
 
-def creation_paquet_0()-> Paquet:
-    """Crée un paquet de 52 cartes
+def creation_paquet_0(nb_cartes:int = 52)-> Paquet:
+    """Crée un paquet de nb_cartes cartes
 
+    Args:
+        nb_cartes (int, optional): Nombre de cartes dans le paquet à retourner.
     Returns:
-        Paquet: Paquet contenant les 52 cartes créées
+        Paquet: Paquet contenant les cartes créées
     """
     paquet_0 = Paquet([])
     for symbole in SYMBOLES:
-        for valeur in range(1,14):
+        for valeur in range(1,nb_cartes//4+1):
             paquet_0.ajouter(Carte(valeur, symbole))
     return paquet_0
 
-def affrontement(paquet_1: Paquet, paquet_2:Paquet):
-    """Compare la valeur de la tête des paquets et déclenche éventuellement une bataille.
+def affrontement(paquet_1: Paquet, paquet_1bis: Paquet, paquet_2: Paquet, paquet_2bis:Paquet)-> int:
+    """Déclenche l'affrontement entre les paquets des deux joueurs
 
     Args:
-        paquet_1 (Paquet): Premier paquet de l'affrontement
-        paquet_2 (Paquet): Second paquet de l'affrontement
-        bataille_poss(bool, optional): Contrôle le déclenchement de la bataille
+        paquet_1 (Paquet): Paquet du premier joueur. Paquet "caché".
+        paquet_1bis (Paquet): Paquet du premier joueur. Paquet "visible".
+        paquet_2 (Paquet): Paquet du second joueur. Paquet "caché".
+        paquet_2bis (Paquet): Paquet du second joueur. Paquet "visible".
+        
+    Returns:
+        int: numéro du joueur gagnant
     """
-    paquet_1bis = Paquet(paquet_1.retirer())
-    paquet_2bis = Paquet(paquet_2.retirer())
     print(paquet_1bis.get_cartes()[-1], paquet_2bis.get_cartes()[-1])
     if (paquet_1bis.get_cartes()[-1])>(paquet_2bis.get_cartes()[-1]):
         paquet_1bis.assembler(paquet_2bis)
@@ -33,26 +37,26 @@ def affrontement(paquet_1: Paquet, paquet_2:Paquet):
         paquet_2.assembler(paquet_2bis)
         return 2
     else:
-        return bataille((paquet_1, paquet_1bis), (paquet_2, paquet_2bis))
+        return bataille(paquet_1, paquet_1bis, paquet_2, paquet_2bis)
 
-def bataille(paquets_1: tuple[Paquet, Paquet], paquets_2: tuple[Paquet, Paquet]):
+def bataille(paquet_1: Paquet, paquet_1bis: Paquet, paquet_2: Paquet, paquet_2bis: Paquet)-> int:
     """Bataille
 
     Args:
-        paquet_1 (tuple[Paquet, Paquet]): Premier couple de paquets de la bataille
-        paquet_2 (tuple[Paquet, Paquet]): Second couple de paquets de la bataille
+        paquet_1 (Paquet): Paquet du premier joueur. Paquet "caché".
+        paquet_1bis (Paquet): Paquet du premier joueur. Paquet "visible".
+        paquet_2 (Paquet): Paquet du second joueur. Paquet "caché".
+        paquet_2bis (Paquet): Paquet du second joueur. Paquet "visible".
+
+    Returns:
+        int: numéro du joueur gagnant
     """
     print("BATAILLE")
-    paquet_1bis = paquets_1[1]
-    paquet_2bis = paquets_2[1]
     for i in range(2):
-        if not paquets_1[0].est_vide():
-            paquet_1bis.recup(paquets_1[0], en_haut= True)
-        if not paquets_2[0].est_vide():
-            paquet_2bis.recup(paquets_2[0], en_haut=True)
-    print(paquet_1bis, paquet_2bis)
-    vainqueur = affrontement(paquet_1bis, paquet_2bis)
-    print(paquet_1bis, paquet_2bis)
-    paquets_1[0].assembler(paquet_1bis)
-    paquets_2[0].assembler(paquet_2bis)
+        if not paquet_1.est_vide():
+            paquet_1bis.recup(paquet_1, en_haut= True)
+        if not paquet_2.est_vide():
+            paquet_2bis.recup(paquet_2, en_haut=True)
+    vainqueur = affrontement(paquet_1, paquet_1bis, paquet_2, paquet_2bis)
+    print("FIN BATAILLE")
     return vainqueur
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from bataille.fonctions.jeu import creation_paquet_0, affrontement
+from bataille.classes.paquet import Paquet
 
 def main():
     paquet_0 = creation_paquet_0()
     paquet_0.melanger()
     paquet_1, paquet_2 = paquet_0.split()
     del paquet_0
+    paquet_1bis = Paquet([])
+    paquet_2bis =  Paquet([])
     vainqueur = None
     manche = 0
     while not (paquet_1.est_vide() or paquet_2.est_vide()):
-        vainqueur = affrontement(paquet_1, paquet_2)
-        print(f"Manche {manche} | Vainqueur : {vainqueur}")
+        print(f"\nManche {manche}")
+        paquet_1bis.recup(paquet_1, en_haut=True)
+        paquet_2bis.recup(paquet_2, en_haut=True)
+        vainqueur = affrontement(paquet_1, paquet_1bis, paquet_2, paquet_2bis)
+        print(f"Vainqueur de la manche: {vainqueur}")
         manche += 1
+    print("\nVAINQUEUR DE LA PARTIE: ", vainqueur)
 
 main()
